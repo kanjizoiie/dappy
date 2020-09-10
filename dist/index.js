0d70627323c7e4335f272ef1f5045a214df5efc4
Marcus Johansson <mjoh9512@gmail.com>
2020-09-10 07:11:59 +0200

    Removed the build artifacts folder from the repo and added the build artifacts folder to the .gitignore file.

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log("Hello, world!");
-function add(A, B) {
-    return A + B;
-}
-console.log("Added 3 and 5, the result is: ", add(3, 5));
-//# sourceMappingURL=index.js.map
